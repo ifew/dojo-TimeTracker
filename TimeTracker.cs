ecdaa1b02b6d42b9236e088d96966934c421b6ef
iFew <chitpong@gmail.com>
2018-02-05 23:10:00 +0700

    Added production code

--- TimeTracker.cs
+++ TimeTracker.cs
@@ -8,8 +8,13 @@ namespace TimeTracker
         public Boolean CheckValid(TimeSpan inputTime)
         {
             TimeSpan timeStart = new TimeSpan(8,0,0);
+            TimeSpan timeEnd = new TimeSpan(17,0,0);
 
-            if(inputTime == timeStart) {
+            if(inputTime >= timeStart) {
+                return true;
+            }
+
+            if(inputTime <= timeEnd) {
                 return true;
             }
 
