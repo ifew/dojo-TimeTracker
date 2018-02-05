0d62bf112078d1eff8764a693b8063589c2ed688
iFew <chitpong@gmail.com>
2018-02-05 23:00:35 +0700

    Added case 0800 return true

--- TimeTrackerTest.cs
+++ TimeTrackerTest.cs
@@ -6,9 +6,14 @@ namespace TimeTracker
     public class TimeTrackerTest
     {
         [Fact]
-        public void Test1()
+        public void When_Time_Is_0800_Should_Be_True()
         {
+            TimeSpan setTime = new TimeSpan(8,0,0);
 
+            TimeTracker timeTracker = new TimeTracker();
+            bool actual = timeTracker.CheckValid(setTime);
+
+            Assert.True(actual);
         }
     }
 }
